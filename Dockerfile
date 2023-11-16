5ed79e3a135b0bb818636a37bc27d72075c09a01
YuliaDyka <93158149+YuliaDyka@users.noreply.github.com>
2023-11-16 22:16:19 +0200

    fixed links and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,11 @@
-FROM alpine/git as clone
-WORKDIR /repo
-RUN git clone --branch lab_2 https://github.com/YuliaDyka/cloud_lab1.git
-
 FROM maven:3.8.3-openjdk-17 as build
-WORKDIR /src
-COPY --from=clone /repo/cloud_lab1 .
-RUN mvn install
+WORKDIR /project-dir
+COPY . .
+RUN mvn -B -DskipTests clean package
 
 FROM openjdk:17-alpine
 WORKDIR /app
-COPY --from=build /src/target/back_end-0.0.1-SNAPSHOT.jar .
+COPY --from=build /project-dir/target/back_end-0.0.1-SNAPSHOT.jar .
 EXPOSE 8080
 ENTRYPOINT ["sh", "-c"]
 CMD ["java -jar back_end-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/api/controller/HomeController.java
+++ src/main/java/com/api/controller/HomeController.java
@@ -10,27 +10,27 @@ public class HomeController {
     @GetMapping(value = "")
     public String GetHomePage() {
         String strOutput = "-------------- GET ALL --------------------<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/films<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/actors<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/budgets<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/cashCollections<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/countries<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/nominations<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/raitings<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/reviewers<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/films<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/actors<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/budgets<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/cashCollections<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/countries<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/nominations<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/raitings<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/reviewers<br>";
         strOutput = strOutput + "-------------- GET BY ID --------------------<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/films/1<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/actors/2<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/budgets/1<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/cashCollections/1<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/countries/1<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/nominations/1<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/raitings/1<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/reviewers/1<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/films/1<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/actors/2<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/budgets/1<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/cashCollections/1<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/countries/1<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/nominations/1<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/raitings/1<br>";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/reviewers/1<br>";
         strOutput = strOutput + "-------------- SWAGGER --------------------<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/swagger-ui/";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/swagger-ui/<br>";
         strOutput = strOutput + "-------------- GET FILM BY NAME --------------------<br>";
-        strOutput = strOutput + "https://films-ratings-yulia.azurewebsites.net/api/films/name?filmName=Rokky";
+        strOutput = strOutput + "https://films-ratings-yulia-container.azurewebsites.net/api/films/name?filmName=Rokky";
         return strOutput;
 
     }
